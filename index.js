a80a9d2fda649555592e1c1a1f3e9dc5540b5415
Jesús Lobos <jlobos@outlook.cl>
2017-07-17 04:52:34 -0400

    add encodeURIComponent

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const fetch = require('node-fetch')
 
-module.exports = ({ key, url }) => {
-  return new Promise((resolve, reject) => {
-    fetch(`http://ouo.io/api/${key}?s=${url}`)
-    .then(res => {
-      if (res.status !== 200) {
-        return reject(Error('Error occurred in fetch API'))
-      }
-
-      res.text().then(link => resolve(link))
-    })
-  })
-}
+module.exports = ({ key, url }) =>
+  fetch(`http://ouo.io/api/${key}?s=${encodeURIComponent(url)}`).then(res =>
+    res.text()
+  )
--- test.js
+++ test.js
@@ -3,7 +3,16 @@ import short from './index'
 
 test('short', async t => {
   t.is(
-    await short({ key: 'kZrfrYdn', url: 'https://www.google.com' }),
-    'http://ouo.io/K60dXX'
+    await short({ key: 'CzTTkWg6', url: 'https://www.google.com' }),
+    'http://ouo.io/xdaasd'
   )
+})
+
+test('short MEGA link', async t => {
+  const result = await short({
+    key: 'CzTTkWg6',
+    url: 'https://mega.nz/#F!lg8WibJY!8auQ5Col_rVfEzG2eaZRDQ'
+  })
+
+  t.is(result, 'http://ouo.io/624Ae')
 })
